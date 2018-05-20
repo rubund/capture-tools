968e36b6dd9dc288bbf5a59083cf0cc66790019e
Ruben Undheim <ruben.undheim@gmail.com>
2018-05-21 01:17:40 +0200

    Print timestamp

--- gr-capture_tools/lib/burst_msg_source_c_impl.cc
+++ gr-capture_tools/lib/burst_msg_source_c_impl.cc
@@ -22,6 +22,7 @@
 #include "config.h"
 #endif
 
+#include <ctime>
 #include <gnuradio/io_signature.h>
 #include "burst_msg_source_c_impl.h"
 
@@ -145,7 +146,10 @@ namespace gr {
                     value = pmt::dict_add(value, pmt::mp("sample_rate"), pmt::from_float(sample_rate));
 
                     if(remaining_in_current > 0) {
-                        std::cout << "Received: Freq: " << (burst_freq/1e6) << " MHz, Magnitude: " << magnitude << std::endl;
+                        std::cout << "Received: Freq: " << (burst_freq/1e6) << " MHz, Magnitude: " << magnitude;
+                        time_t t = time(NULL);
+                        struct tm tm = *localtime(&t);
+                        fprintf(stdout, ", Time: %04d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
                         add_item_tag(0, nitems_written(0) + produced, key, value);
                     }
                 }
